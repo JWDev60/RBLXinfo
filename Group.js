b307d763164ba54ba9a004cdea56f38bbfa5fa75
JWDev60 <165835751+JWDev60@users.noreply.github.com>
2024-04-13 20:08:13 +0100

    Update Group.js

--- Group.js
+++ Group.js
@@ -1,20 +1,33 @@
-const groupId = 123456; // Replace with your group ID
+const fetch = require('node-fetch');
 
-fetch(`https://groups.roblox.com/v1/groups/${groupId}`)
-  .then(response => response.json())
-  .then(data => {
-    // Access the group information from the response data
-    const groupName = data.name;
-    const groupDescription = data.description;
-    const groupOwner = data.owner;
-    const groupEmblemUrl = data.emblemUrl;
+// Replace 'GROUP_ID' with the actual group ID you want to retrieve information for
+const groupId = 'GROUP_ID';
 
-    // Do something with the group information
-    console.log(`Group Name: ${groupName}`);
-    console.log(`Group Description: ${groupDescription}`);
-    console.log(`Group Owner: ${groupOwner}`);
-    console.log(`Group Emblem URL: ${groupEmblemUrl}`);
-  })
-  .catch(error => {
-    console.error('Error:', error);
+// Function to make the API request and retrieve group information
+// Replace 'YOUR_AUTHORIZATION_TOKEN' with the Authorization Token You Have Made
+async function getGroupInfo(groupId) {
+  const url = `https://groups.roblox.com/v1/groups/${groupId}`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: 'YOUR_AUTHORIZATION_TOKEN'
+    }
   });
+  const groupInfo = await response.json();
+
+  // Extract the required group information
+  const ownerName = groupInfo.owner.userDisplayName;
+  const createdDate = groupInfo.created;
+  const memberCount = groupInfo.memberCount;
+  const gamesCount = groupInfo.publicEntryAllowedGamesCount;
+  const ranksCount = groupInfo.roles.length;
+
+  // Print the group information
+  console.log('Owner Name:', ownerName);
+  console.log('Group Creating Date:', createdDate);
+  console.log('Amount Of Group Members:', memberCount);
+  console.log('Amount Of Published Games:', gamesCount);
+  console.log('Amount Of Group Ranks:', ranksCount);
+}
+
+// Call the function with the group ID
+getGroupInfo(groupId);
